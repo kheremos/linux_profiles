2f0568c29b364ad197388a2fc9148d0499c1871a
gregg <gregg@gtsservices.com>
2015-06-25 16:42:28 -0400

    Added an install script to do the copying.

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script to "install" the profile configuration
+
+mv ~/.bash_profile backup/
+mv ~/.bashrc backup/
+mv ~/.bash_aliases backup/
+cp .bash_profile ~
+cp .bashrc ~
+cp .bash_aliases ~
