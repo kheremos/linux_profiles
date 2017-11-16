f489c24c78d88e14194789d04d8499578cf501c3
kheremos <kheremos@gmail.com>
2017-11-16 08:54:02 -0800

    Update handy_tools.sh
    Added a note on one-line encryption/decryption.

--- notes/handy_tools.sh
+++ notes/handy_tools.sh
@@ -12,3 +12,9 @@ apt-get install keychain
 # https://github.com/ggreer/the_silver_searcher
 apt-get install silversearcher-ag
 # brew install ag
+
+
+# Quick Encrypt/Decrypt examples
+#openssl des3 -in file.txt -out encrypted.txt
+#openssl des3 -d -in encrypted.txt -out normal.txt
+
